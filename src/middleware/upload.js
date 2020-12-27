fa71220c56fc8e25318be9f2d7db696d479e7860
s1ga <s.v.harlanov@gmail.com>
2020-12-27 16:18:13 +0300

    feat: implement uploading avatar

---
+++ src/middleware/upload.js
@@ -0,0 +1,33 @@
+const multer = require('multer')
+const path = require('path')
+const moment = require('moment')
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, path.join(__dirname, 'uploads'))
+    },
+    filename(req, file, cb) {
+        const date = moment().format('DDMMYYYY-HHmmss_SSS')
+        cb(null, `${date}-${file.originalname}`)
+    }
+})
+
+const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg']
+
+const fileFilter = (req, file, cb) => {
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+const limits = {
+    fileSize: 1024 * 1024 * 5
+}
+
+module.exports = multer({
+    storage,
+    fileFilter,
+    limits
+})
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const controller = require('../controller/auth')
 const { registerValidator } = require('../utils/validator')
+const upload = require('../middleware/upload')
 
-router.post('/register', registerValidator, controller.register)
+router.post('/register', registerValidator, upload.single('image'), controller.register)
 router.post('/login', controller.login)
 
 module.exports = router
