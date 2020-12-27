cd32d589f564ccdaac5912aa06219a89878ba14f
s1ga <s.v.harlanov@gmail.com>
2020-12-27 13:12:36 +0300

    fix: fix auth routes path

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const controller = require('../controller/auth')
 
-router.post('/', controller.register)
-router.post('/', controller.login)
+router.post('/register', controller.register)
+router.post('/login', controller.login)
 
 module.exports = router
